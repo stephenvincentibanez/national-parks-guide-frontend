bf71e9a35c456d39ea074557f5a0b4d790245933
Steve <stephenvincentibanez@gmail.com>
2020-12-01 17:32:30 -0800

    dropdown menu no map :(

--- index.js
+++ index.js
@@ -1,44 +1,52 @@
 
-    const PARKS_URL = "http://localhost:3000/parks"
-    const map = document.getElementById("map")
-    map.addEventListener('click', (e)=>handleClick(e))
+    const PARKS_URL = "http://localhost:3000/"
+    // const map = document.getElementById("map")
+    // map.addEventListener('click', (e)=>handleMapClick(e))
     const search = document.getElementById("search")
     search.addEventListener('submit', (e)=>handleSearch(e))
+    const stateList = document.getElementById("state-dropdown")
+    stateList.addEventListener('submit', (e)=>handleSubmit(e))
     
     function getParks(){
         fetch(PARKS_URL)
         .then(res => res.json())
-        .then(parks => iterateParks(parks))
+        // .then(parks => iterateParks(parks))
+        .then(console.log)
     }
     getParks()
 
-    function iterateParks(parks){
-        for(const park of parks){
-            handleClick(park)
-            // handleClick(park)
-            // console.log(park)
-        }
-    }
+    // function iterateParks(parks){
+    //     for(const park of parks){
+    //         buildLi(park)
+    //     }
+    // }
 
-    function buildLi(park){
-        const ul = document.getElementById("list")
-        const li = document.createElement("li")
-        li.innerHTML = park.name
-        ul.appendChild(li)
-    }
+    // function buildLi(park){
+    //     const ul = document.getElementById("park-list")
+    //     const li = document.createElement("li")
+    //     li.innerHTML = park.name
+    //     ul.appendChild(li)
+    // }
 
-    function handleClick(e, park){
-        console.log(park)
+    function handleSubmit(e){
         e.preventDefault()
-        console.log(e.target.textContent)
-        if (e.target.textContent === park.states){
-            buildLi(park)
-        } 
-        // if the innerText of target == Park.states, show those parks in an li
-        //make a fetch function that changes depending on which state is clicked?
+        const state = e.target.states.value
+        fetch(PARKS_URL + `park_search?query=${state}`)
+        .then(res => res.json())
+        .then(console.log)
     }
-    handleClick()
 
+    // function handleMapClick(e){
+  
+    //     console.log(e.target)
+    //     console.log(e.target.textContent)
+    //     if (e.target.textContent === park.states){
+    //         buildLi(park)
+    //     } 
+    //     // if the innerText of target == Park.states, show those parks in an li
+    //     //make a fetch function that changes depending on which state is clicked?
+    // }
+   
     function handleSearch(e){
         e.preventDefault()
         const query = e.target.query.value
