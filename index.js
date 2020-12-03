fc134c3c83d988c8e311c1cd1ba393846e1f62c9
Steve <stephenvincentibanez@gmail.com>
2020-12-02 16:43:49 -0800

    Park index and show pages with images appearing. users posting to database though with a syntax error in console

--- index.js
+++ index.js
@@ -1,55 +1,135 @@
+    const URL = "http://localhost:3000/"
 
-    const PARKS_URL = "http://localhost:3000/"
-    // const map = document.getElementById("map")
-    // map.addEventListener('click', (e)=>handleMapClick(e))
-    const search = document.getElementById("search")
-    search.addEventListener('submit', (e)=>handleSearch(e))
+    // const search = document.getElementById("search")
+    // search.addEventListener('submit', (e)=>handleSearch(e))
+
     const stateList = document.getElementById("state-dropdown")
-    stateList.addEventListener('submit', (e)=>handleSubmit(e))
+    stateList.addEventListener('submit', (e)=>handleStateSubmit(e))
     
-    function getParks(){
-        fetch(PARKS_URL)
-        .then(res => res.json())
-        // .then(parks => iterateParks(parks))
+    const parksContainer = document.getElementById("parks-container")
+    const photoContainer = document.getElementById("photo-container")
+    const signUpForm = document.getElementById('sign-up')
+    signUpForm.addEventListener("submit", (e) => postUser(e))
+    const signUpDiv = document.getElementById('sign-up-div')
+
+    function postUser(e){
+        e.preventDefault()
+        const username = e.target.username.value
+        fetch(URL + "users", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json"
+            },
+            body: JSON.stringify({
+                username: username
+            })
+        })
+        .then(r => r.json())
         .then(console.log)
     }
-    getParks()
 
-    // function iterateParks(parks){
-    //     for(const park of parks){
-    //         buildLi(park)
-    //     }
-    // }
-
-    // function buildLi(park){
-    //     const ul = document.getElementById("park-list")
-    //     const li = document.createElement("li")
-    //     li.innerHTML = park.name
-    //     ul.appendChild(li)
-    // }
-
-    function handleSubmit(e){
+    function handleStateSubmit(e){
         e.preventDefault()
         const state = e.target.states.value
-        fetch(PARKS_URL + `park_search?query=${state}`)
+        fetch(URL + `park_search?query=${state}`)
         .then(res => res.json())
-        .then(console.log)
+        .then(parks => iterateParks(parks))
     }
 
-    // function handleMapClick(e){
-  
-    //     console.log(e.target)
-    //     console.log(e.target.textContent)
-    //     if (e.target.textContent === park.states){
-    //         buildLi(park)
-    //     } 
-    //     // if the innerText of target == Park.states, show those parks in an li
-    //     //make a fetch function that changes depending on which state is clicked?
-    // }
-   
-    function handleSearch(e){
-        e.preventDefault()
-        const query = e.target.query.value
-        fetch(PARKS_URL + `/`)
-        //use that query thing from yesterday discussion question in fetch request
+    function iterateParks(parks){
+        parksContainer.innerHTML = ""
+        for(const park of parks){
+            buildLi(park)
+        }
     }
+
+    function buildLi(park){
+        const card = document.createElement("div")
+        card.className = "card"
+
+        let image = park.images.split(" ").find(element => element.includes("url"))
+        image = image.slice(8)
+        image = image.substring(0, image.length - 3)
+
+        card.innerHTML = `
+        <div class="card-image">
+            <figure class="image is-4by3">
+            <img class="list-image"src="${image}" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="media">
+            <div class="media-left">
+            </div>
+            <div class="media-content">
+                <p class="title is-4">${park.name}</p>
+            </div>
+            </div>
+        </div>
+        <br>
+        `
+        parksContainer.appendChild(card)
+        card.addEventListener('click', () => showPark(park))
+    }
+
+    function showPark(park){
+        // photoContainer.innerHTML = ""
+        // signUpDiv.innerHTML = ""
+        let image = park.images.split(" ").find(element => element.includes("url"))
+        image = image.slice(8)
+        image = image.substring(0, image.length - 3)
+        console.log(image)
+        parksContainer.innerHTML = ""
+        parkShow = document.createElement("div")
+        parkShow.className = "card"
+        parkShow.innerHTML = `
+        <div class="card-image">
+        <figure class="image is-4by3">
+          <img class="list-image" src="${image}" alt="Placeholder image">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">${park.name}</p>
+            <p class="subtitle is-6">States: ${park.states}</p>
+            <p> ${park.designation} </p>
+          </div>
+        </div>
+        <div class="content">
+          <p> Location: ${park.latlong}</p>
+          <p class="title is-4">Description: ${park.description}</p>
+          <p> Weather Info: ${park.weather_info}</p>
+        </div>
+      </div>
+        `
+
+        
+
+        const reviewForm = document.createElement("form")
+        reviewForm.id = "review"
+        reviewForm.innerHTML = `
+        <label> Username: </label><br>
+        <input name="username" type="text"><br>
+        <label> Review: </label><br>
+        <input name="review" type="textarea"><br>
+        <input type="submit">
+        `
+        reviewForm.addEventListener('submit', (e) => postReview(e))
+        parksContainer.append(parkShow, reviewForm)
+    }
+
+    function postReview(e){
+        e.preventDefault()
+        const username = e.target.username.value
+        const review = e.target.review.value 
+        fetch(URL + "reviews")
+    }
+   
+    // function handleSearch(e){
+    //     e.preventDefault()
+    //     const query = e.target.query.value
+    //     fetch(PARKS_URL + `/`)
+    //     //use that query thing from yesterday discussion question in fetch request
+    // }
